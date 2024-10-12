4dc79575ece2b125f5746c28befd1a9e6b729d9e
Fsoriano <maxonairos@gmail.com>
2024-10-12 00:38:34 -0300

    -se agrega componente para pagina 404 y estilos

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ItemDetailContainer from './components/ItemDetailContainer'
+import NotFound from './components/NotFound'
 
 function App() {
   
@@ -14,7 +15,7 @@ function App() {
           <Route path= "/" element={<ItemListContainer greeting="¡Bienvenidos a GamerStore! Estamos emocionados de tenerte aquí en tu nuevo destino para todo lo relacionado con el mundo de los videojuegos y la tecnología. En GamerStore, encontrarás una amplia gama de componentes de PC, periféricos y todo lo necesario para llevar tu experiencia gamer al siguiente nivel. Ya sea que busques mejorar tu configuración actual o equiparte desde cero, estamos aquí para ofrecerte los mejores productos y asesoramiento. ¡Explora nuestra tienda y prepárate para jugar en grande!"/>}/>  
           <Route path= "/categoria/:idCategory" element={<ItemListContainer/>}/>
           <Route path= "/detalle/:idProduct" element={<ItemDetailContainer/>}/>
-          <Route path= "*" element={<div>Error 404 - la pagina no existe</div>}/>
+          <Route path= "*" element={<NotFound/>}/>
           
         </Routes>  
           
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+
+const NotFound = () => {
+  return (
+    <div className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-bold text-subtle-600">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Pagina no encontrada</h1>
+          <p className="mt-6 text-base leading-7 text-gray-600">Disculpanos, pero no encontramos la pagina que estás buscando.</p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Link className="rounded-md bg-success-subtle px-3.5 py-2.5 text-sm font-semibold text-grey-500 shadow-sm hover:hover:bg-gray-700 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-700 " to="/">Volver al Inicio</Link>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default NotFound
