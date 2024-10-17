ea93f1664440d37b96998c58aa114c0d1192df96
Fsoriano <maxonairos@gmail.com>
2024-10-17 15:13:12 -0300

    -se mejora funcionalidades generales: -se agrega link al detalle de cada producto dentro del Cart. -se agregan botones personalizados. -se mejora el estilo y el orden de algunos componentes.

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,12 +1,9 @@
-//debe tener boton para aumentar y restar, no debe llegar a 0 ni en numero en negativo
-
-//pasar como prop el stock maximo
-//boton agregar al carrito, mostrar el boton actual del contador
-import { useState, useContext  } from "react"
+import { useState  } from "react"
+import { FaCirclePlus } from "react-icons/fa6";
+import { FaCircleMinus } from "react-icons/fa6";
 
 export const ItemCount = ({stock, addProduct}) => {
   const [count, setCount ] = useState(1)
-  //const { addProductInCart  } = useContext(CartContext)
 
   
 
@@ -29,16 +26,16 @@ export const ItemCount = ({stock, addProduct}) => {
   }
 
   return (
-    <div className="w-40 flex-col justify-center rounded-xl bg-grey border">
+    <div className="w-48 flex-col justify-center rounded-xl bg-grey border">
       <div className='flex justify-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white underline underline-offset-4'>Cantidad</div>
       <div className="flex justify-center py-2">
-        <button className="inline-flex justify-center px-3 py-3 text-sm font-medium text-center text-white bg-seagreen rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={handleClickDecrement}>-</button>
-        <p className="inline-flex px-8 py-2 justify-center rounded-xl bg-grey border text-justify font-bold">{count}</p>
-        <button className="inline-flex items-end px-3 py-3 text-sm font-medium text-center text-white bg-seagreen rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={handleClickIncrement}>+</button>
+        <button className="inline-flex justify-center px-2 py-2 text-sm font-medium text-center text-white bg-seagreen rounded-xl hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={handleClickDecrement}>{<FaCircleMinus size={30}/>}</button>
+        <p className="inline-flex px-8 p-2 justify-center rounded-xl bg-grey border text-justify font-bold">{count}</p>
+        <button className="inline-flex items-center p-2 text-sm font-medium text-center text-white bg-seagreen rounded-xl hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={handleClickIncrement}>{<FaCirclePlus size={30}/>}</button>
         </div>
       
       <div className="flex py-2 justify-center">
-      <button className="inline-flex items-end px-3 py-2 text-sm font-medium text-center text-white bg-seagreen rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={handleClickAdd}>Agregar al Carrito</button>
+      <button className="inline-flex w-38 items-end px-3 py-2 text-sm font-medium text-center text-white bg-seagreen rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={handleClickAdd}>Agregar al Carrito</button>
       </div>
       
     </div>
--- src/components/NavBar/Cart.jsx
+++ src/components/NavBar/Cart.jsx
@@ -1,27 +1,35 @@
 import { useContext } from "react"
 import { CartContext } from "../../context/CartContext"
+import { FaDeleteLeft } from "react-icons/fa6"
+import { Link } from "react-router-dom"
+
 
 const Cart = () => {
-const { cart, totalPrice, deleteProductInCart, deleteCart } = useContext(CartContext)
-
+const { cart, totalPrice, totalQuantity, deleteProductInCart, deleteCart } = useContext(CartContext)
     return (
-        <div>
-            <h1>Mi Carrito</h1>
+        <div className="flex flex-wrap justify-center ">
+            
+            <div className="flex flex-col ">
+            <h1 className='flex flex-wrap self-center mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white'>Mi Carrito</h1>
             {
                 cart.map((productCart)=>(
-                    <div key={productCart.id}>
-                        <img className="flex flex-wrap m-1 bg-grey border hover:opacity-50 rounded-md object-contain h-48 w-48 sm:object-scale-down" src={productCart.img[0]} alt="Imagen Producto" />
-                        <p>Nombre: {productCart.nombre}</p>
-                        <p>Precio: {productCart.precio}</p>
-                        <p>Cantidad: {productCart.quantity}</p>
-                        <p>Precio Parcial: {productCart.precio * productCart.quantity}</p>
-                        <button onClick={()=>deleteProductInCart(productCart.id)}>Eliminar Producto</button>
+                    <div className="grid gap-4 grid-cols-6 justify-center m-3 p-2 max-w-6xl bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-70" key={productCart.id}>
+                        <Link to={`/detalle/${productCart.id}`}>
+                        <img className="flex m-1 bg-grey border hover:opacity-50 rounded-md object-contain h-48 w-48 sm:object-scale-down" src={productCart.img[0]} alt="Imagen Producto" />
+                        </Link>
+                        
+                        <p className='self-center mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white'>{productCart.nombre}</p>
+                        <p className='self-center mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white'>Precio: {productCart.precio}</p>
+                        <p className='self-center mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white'>Cantidad: {productCart.quantity}</p>
+                        <p className='self-center mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white'>Precio Parcial: {productCart.precio * productCart.quantity} ARS</p>
+                        <button onClick={()=>deleteProductInCart(productCart.id)}>{<FaDeleteLeft size={30} color="seagreen"/>}</button>
                     </div>
                 ))
             }
             <div>
                 <p>Precio Total: {totalPrice()} ARS</p>
                 <button onClick={deleteCart} >Vaciar Carrito</button>
+            </div>
             </div>
         </div>
     )
