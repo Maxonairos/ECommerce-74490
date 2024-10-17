f1e75d88cb40071edd0f97a78b1feab916c336bb
Fsoriano <maxonairos@gmail.com>
2024-10-17 10:18:47 -0300

    -se corrige nombre de funcion manejadora

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -23,7 +23,7 @@ export const ItemCount = ({stock, addProduct}) => {
     }
   }
 
-  const addAndReset = ()=>{ 
+  const handleClickAdd = ()=>{ 
     addProduct(count);
     setCount(1)
   }
@@ -38,7 +38,7 @@ export const ItemCount = ({stock, addProduct}) => {
         </div>
       
       <div className="flex py-2 justify-center">
-      <button className="inline-flex items-end px-3 py-2 text-sm font-medium text-center text-white bg-seagreen rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={ addAndReset }>Agregar al Carrito</button>
+      <button className="inline-flex items-end px-3 py-2 text-sm font-medium text-center text-white bg-seagreen rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={handleClickAdd}>Agregar al Carrito</button>
       </div>
       
     </div>
