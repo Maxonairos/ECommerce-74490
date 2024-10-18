064e79e8ac19d59a4cde865cdf8c7a0088e67b6b
Fsoriano <maxonairos@gmail.com>
2024-10-18 15:40:05 -0300

    -se ordena codigo

--- src/components/NavBar/CartWidgetMobile.jsx
+++ src/components/NavBar/CartWidgetMobile.jsx
@@ -1,4 +1,3 @@
-import { FaShoppingCart } from "react-icons/fa";
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 
@@ -7,8 +6,9 @@ const CartWidgetMobile = () => {
   const { totalQuantity } = useContext(CartContext)
 
   return (
-    <div className="inline-flex items-center rounded-md bg-white text-sm px-2 font-black text-black-700 ring-1 ring-inset ring-white-900">{totalQuantity()}</div>
-    
+    <div className="inline-flex items-center rounded-md bg-white text-sm px-2 font-black text-black-700 ring-1 ring-inset ring-white-900">
+        {totalQuantity()}
+    </div>
   )
 }
 
