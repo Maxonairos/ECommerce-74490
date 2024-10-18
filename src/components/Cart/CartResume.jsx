765079def6bf52ef018e06e2312f80e1299370a8
Fsoriano <maxonairos@gmail.com>
2024-10-18 14:53:26 -0300

    -se ordena codigo

--- src/components/Cart/CartResume.jsx
+++ src/components/Cart/CartResume.jsx
@@ -6,29 +6,29 @@ const CartResume = () => {
     const {totalPriceCart, deleteCart, totalQuantity} = useContext(CartContext)
 
     const modalWarningAll =()=>{
-      Swal.fire({
-          icon: "question",
-          title: "Seguro que quieres Eliminar el carrito?",
-          text: "Tendrás que agregar todos los productos nuevamente luego",
-          confirmButtonColor: "#00916E",
-          confirmButtonText: "Vaciar Carrito",
-          focusCancel: true,
-          showCancelButton: true,
-          cancelButtonText: "Cancelar",
-      }).then((resultado)=>{
-          if (resultado.isConfirmed) {
-              deleteCart() 
-          }
-      })
-  }
+        Swal.fire({
+        icon: "question",
+        title: "Seguro que quieres Eliminar el carrito?",
+        text: "Tendrás que agregar todos los productos nuevamente luego",
+        confirmButtonColor: "#00916E",
+        confirmButtonText: "Vaciar Carrito",
+        focusCancel: true,
+        showCancelButton: true,
+        cancelButtonText: "Cancelar",
+        }).then((resultado)=>{
+            if (resultado.isConfirmed) {
+            deleteCart() 
+            }
+        })
+    }
 
-  return (
+    return (
     <div className="m-3 p-2 max-w-6xl bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-70 ">
-        <p className='self-center mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white'>Precio Total: {totalPriceCart()} ARS</p>
+        <p className='self-center mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white'>Precio Total: {totalPriceCart()}$ ARS</p>
         <p className='self-center mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white'>Total: {totalQuantity()} Productos</p>
         <button className="inline-flex self-end px-3 py-2 text-sm font-medium text-center text-white bg-seagreen rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={modalWarningAll} >Vaciar Carrito</button>
     </div>
-  )
+    )
 }
 
 export default CartResume
