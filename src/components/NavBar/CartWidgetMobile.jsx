87eb4ab4d4e18d9b73c947d221c9da5ab4d61ac3
Fsoriano <maxonairos@gmail.com>
2024-10-18 15:37:44 -0300

    -se quita icono de cart para mobile

--- src/components/NavBar/CartWidgetMobile.jsx
+++ src/components/NavBar/CartWidgetMobile.jsx
@@ -7,11 +7,8 @@ const CartWidgetMobile = () => {
   const { totalQuantity } = useContext(CartContext)
 
   return (
+    <div className="inline-flex items-center rounded-md bg-white text-sm px-2 font-black text-black-700 ring-1 ring-inset ring-white-900">{totalQuantity()}</div>
     
-    <div className="inline-flex items-center">
-        <FaShoppingCart size={20} color="grey"/>
-        <div className="inline-flex items-center rounded-md bg-white text-sm px-2 font-black text-black-700 ring-1 ring-inset ring-white-900">{totalQuantity()}</div>
-    </div>
   )
 }
 
