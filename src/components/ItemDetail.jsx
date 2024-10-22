4bf1197bb61aa118bc83565baeb659f235d694a9
Fsoriano <maxonairos@gmail.com>
2024-10-22 15:23:09 -0300

    -se quita clases innecesarias

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -8,7 +8,7 @@ const ItemDetail = ({ product , addProduct, hideItemCount , removeButtonFinishBu
   const images = product.img.filter ( (imagen) => imagen !== currentImage )
 
   return (
-    <div className='main flex justify-center max-w-9xl'>
+    <div className='flex justify-center max-w-9xl'>
       <div className="justify-center m-3 p-2 max-w-4xl bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <h2 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white underline underline-offset-4'>{product.nombre}</h2>
               <div className="justify-center flex">
