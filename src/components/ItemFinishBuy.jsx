8e7a4d57877dbfb77ffe6c09bc916aacac4a4b44
Fsoriano <maxonairos@gmail.com>
2024-10-22 15:25:18 -0300

    -se corrige botonera para que sea responsive

--- src/components/ItemFinishBuy.jsx
+++ src/components/ItemFinishBuy.jsx
@@ -7,8 +7,8 @@ const ItemFinishBuy = ({removeButtonFinishBuy }) => {
 
     return (
             <div className="flex flex-wrap justify-evenly">
-                    <Link to="/cart" className="inline-flex w-38 items-center  h-10 px-3 py-2 text-sm font-medium text-center text-white bg-seagreen rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Terminar mi Compra</Link>
-                    <button className="inline-flex w-38 items-center px-3 py-2 text-sm font-medium text-center text-white bg-seagreen rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={handleClickKeepAdd}>Seguir Agregando</button>
+                    <Link to="/cart" className="inline-flex w-38 items-center  h-10 m-2 px-3 py-2 text-sm font-medium text-center text-white bg-seagreen rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Terminar mi Compra</Link>
+                    <button className="inline-flex w-38 items-center h-10 m-2 px-3 py-2 text-sm font-medium text-center text-white bg-seagreen rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={handleClickKeepAdd}>Seguir Agregando</button>
             </div>        
     )
 }
