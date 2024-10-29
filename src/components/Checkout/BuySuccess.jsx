9836c45e5fb4889a3dc7eb7bb05c2ce7d2c2e4b8
Fsoriano <maxonairos@gmail.com>
2024-10-29 16:09:11 -0300

    -se mejora estilo y mensajes

--- src/components/Checkout/BuySuccess.jsx
+++ src/components/Checkout/BuySuccess.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom"
 
 const BuySuccess = ({orderId}) => {
     return (
-        <div className="max-w-2xl bg-white rounded-lg shadow-md p-8">
-            <h2 className="text-2xl font-bold text-gray-800 mb-4">Tu orden ha sido enviada correctamente 😀</h2>
+        <div className="max-w-2xl bg-white rounded-lg shadow-md p-8 m-2">
+            <h2 className="text-2xl font-bold text-gray-800 mb-4">Tu compra fue Exitosa!!! 😀</h2>
             <p className="text-xl font-bold text-gray-900 mb-4">Puedes guardar el numero de seguimiento para obtener información de tu compra</p>
-            <p className="inline-flex text-xl font-bold text-gray-900 mb-4">Orden:</p>
-            <p className="inline-flex text-xl font-bold text-gray-500 mb-4">"{orderId}"</p>
+            <p className="inline-flex text-xl font-bold text-gray-900 mb-4">Orden Nº:</p>
+            <p className="inline-flex text-xl font-medium text-green-600 mb-4">"{orderId}"</p>
             <div className="">
                 <Link className="inline-flex self-center m-1 px-3 py-2 text-sm font-medium text-center text-white bg-gradient-to-r from-green-500 to-seagreen rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" to="/">Volver al Inicio</Link>
             </div>
